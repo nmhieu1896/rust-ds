d15ad40a33da517e9ec93c640bbbdf23d1ce5764
Hieu Nguyen <nmhieu1896@gmail.com>
2023-12-28 22:39:07 +0700

    shorten previous solution and adding bitmap-as-key solution

--- src/hash/group_anagrams.rs
+++ src/hash/group_anagrams.rs
@@ -12,6 +12,10 @@ pub fn _run() {
         let result = group_anagrams(test_case.clone());
         println!("result {:?}", result);
     }
+    for test_case in test_cases.iter() {
+        let result = group_anagrams_2(test_case.clone());
+        println!("result {:?}", result);
+    }
 }
 
 fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
@@ -19,17 +23,35 @@ fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     for text in strs.iter() {
         let mut chars = text.clone().chars().collect::<Vec<char>>();
         chars.sort();
-        map.entry(chars)
-            .and_modify(|v| {
-                v.push(text.to_string());
-            })
-            .or_insert(vec![text.to_string()]);
+        map.entry(chars).or_insert(vec![]).push(text.to_string());
     }
-    let result: Vec<Vec<String>> = map.values().into_iter().map(|v| v.clone()).collect();
+    let result: Vec<Vec<String>> = map.values().cloned().collect();
 
     return result;
 }
 
 fn to_vec_string(vec: Vec<&str>) -> Vec<String> {
     return vec.iter().map(|s| s.to_string()).collect();
+}
+
+const N_LETTERS: usize = (b'z' - b'a' + 1) as _;
+
+pub fn group_anagrams_2(strs: Vec<String>) -> Vec<Vec<String>> {
+    strs.into_iter()
+        .fold(
+            HashMap::<[u8; N_LETTERS], Vec<String>>::new(),
+            |mut map, s| {
+                let freqs = s.bytes().map(|b| (b - b'a') as usize).fold(
+                    [0; N_LETTERS],
+                    |mut freqs, bin| {
+                        freqs[bin] += 1;
+                        freqs
+                    },
+                );
+                map.entry(freqs).or_default().push(s);
+                map
+            },
+        )
+        .into_values()
+        .collect()
 }

85b2e4c67718de2b21697a6868a4d54e90eb8c98
Hieu Nguyen <nmhieu1896@gmail.com>
2023-12-29 22:25:54 +0700

    update four_sum a bit by remove steps

--- src/hash/four_sum_2.rs
+++ src/hash/four_sum_2.rs
@@ -12,25 +12,19 @@ pub fn _run() {
 
 pub fn four_sum_count(nums1: Vec<i32>, nums2: Vec<i32>, nums3: Vec<i32>, nums4: Vec<i32>) -> i32 {
     let mut map1 = HashMap::<i32, i32>::new();
-    let mut map2 = HashMap::<i32, i32>::new();
-    for item_v1 in nums1.iter() {
-        for item_v2 in nums2.iter() {
-            let sum = item_v1 + item_v2;
-            map1.entry(sum).and_modify(|e| *e += 1).or_insert(1);
+
+    for &item_v1 in nums1.iter() {
+        for &item_v2 in nums2.iter() {
+            *map1.entry(item_v1 + item_v2).or_insert(0) += 1;
         }
     }
-    for item_v3 in nums3.iter() {
-        for item_v4 in nums4.iter() {
-            let sum = item_v3 + item_v4;
-            map2.entry(sum).and_modify(|e| *e += 1).or_insert(1);
-        }
-    }
+
     let mut total = 0;
-    map1.keys().into_iter().for_each(|&k| {
-        if map2.contains_key(&(-k)) {
-            total += map1.get(&k).unwrap() * map2.get(&(-k)).unwrap()
+    for &item_v3 in nums3.iter() {
+        for &item_v4 in nums4.iter() {
+            total += map1.get(&(-item_v3 - item_v4)).unwrap_or(&0)
         }
-    });
+    }
 
     return total;
 }

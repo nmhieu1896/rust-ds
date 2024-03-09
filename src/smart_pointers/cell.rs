f817c6db1f2b44bf51bb3be4e183d4ba251240a6
Hieu Nguyen <nmhieu1896@gmail.com>
2024-03-09 23:11:34 +0700

    update clone and test

--- src/smart_pointers/cell.rs
+++ src/smart_pointers/cell.rs
@@ -5,17 +5,30 @@ pub struct Cell<T> {
     value: UnsafeCell<T>,
 }
 
+// impl<T> !Sync for Cell<T> {}
+
 #[allow(dead_code)]
-impl<T: Copy> Cell<T> {
+impl<T> Cell<T> {
     pub fn new(value: T) -> Self {
         Cell {
             value: UnsafeCell::new(value),
         }
     }
 
-    pub fn get(&self) -> T {
+    pub fn get(&self) -> T
+    where
+        T: Copy,
+    {
         unsafe { *self.value.get() }
     }
+
+    pub fn get_clone(&self) -> T
+    where
+        T: Clone,
+    {
+        unsafe { (*self.value.get()).clone() }
+    }
+
     pub fn set(&self, value: T) {
         unsafe { *self.value.get() = value };
     }
@@ -28,6 +41,13 @@ mod test_cell {
     #[test]
     fn test1() {
         let x = Cell::new(10);
-        assert_eq!(x.get(), 10)
+        assert_eq!(x.get(), 10);
+        x.set(15);
+        assert_eq!(x.get(), 15);
+
+        let y = Cell::new(vec![10]);
+        assert_eq!(y.get_clone(), vec![10]);
+        y.set(vec![10, 15, 20]);
+        assert_eq!(y.get_clone(), vec![10, 15, 20]);
     }
 }

63d84bee0f27c80631482bb7db9cd2b55aaa106a
Hieu Nguyen <nmhieu1896@gmail.com>
2024-03-13 22:46:11 +0700

    update rc

--- src/smart_pointers/rc.rs
+++ src/smart_pointers/rc.rs
@@ -1,8 +1,9 @@
+use super::cell::Cell;
 use std::ops::Deref;
 
 struct RcInner<T> {
     value: T,
-    ref_count: usize,
+    ref_count: Cell<usize>,
 }
 
 #[allow(dead_code)]
@@ -10,11 +11,12 @@ pub struct Rc<T> {
     inner: *const RcInner<T>,
 }
 
+#[allow(dead_code)]
 impl<T> Rc<T> {
     pub fn new(v: T) -> Self {
         let inner = Box::new(RcInner {
             value: v,
-            ref_count: 1,
+            ref_count: Cell::new(1),
         });
         Rc {
             inner: Box::into_raw(inner),
@@ -25,7 +27,8 @@ impl<T> Rc<T> {
 impl<T> Clone for Rc<T> {
     fn clone(&self) -> Self {
         let inner = unsafe { &*self.inner };
-        inner.ref_count += 1;
+        let c = inner.ref_count.get();
+        inner.ref_count.set(c + 1);
         Rc { inner: self.inner }
     }
 }
@@ -35,5 +38,18 @@ impl<T> Deref for Rc<T> {
 
     fn deref(&self) -> &Self::Target {
         &unsafe { &*self.inner }.value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rc() {
+        let rc = Rc::new(42);
+        let rc2 = rc.clone();
+        assert_eq!(*rc, 42);
+        assert_eq!(*rc2, 42);
     }
 }

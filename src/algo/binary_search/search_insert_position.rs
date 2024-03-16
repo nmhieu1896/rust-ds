884166fb862b64f1a7e003cbbeffa0a0f369d4b8
Hieu Nguyen <nmhieu1896@gmail.com>
2024-03-16 15:28:54 +0700

    add sip

--- src/algo/binary_search/search_insert_position.rs
+++ src/algo/binary_search/search_insert_position.rs
@@ -3,22 +3,24 @@ pub fn search_insert_position(nums: Vec<i32>, target: i32) -> i32 {
     let mut right = nums.len() - 1;
     let mut left = 0;
     loop {
-        let mid = (right + left) - 1;
+        let mid = (right + left) / 2;
+        if right == left || right == left + 1 {
+            if target > nums[right] {
+                return (right + 1) as i32;
+            }
+            if target <= nums[left] {
+                return left as i32;
+            }
+            return (left + 1) as i32;
+        }
+
         if nums[mid] > target {
-            if left == mid || mid == right {
-                return nums.len() as i32;
-            }
             right = mid;
-            continue;
-        }
-        if nums[mid] < target {
-            if left == mid || mid == right {
-                return nums.len() as i32;
-            }
+        } else if nums[mid] < target {
             left = mid;
-            continue;
+        } else {
+            return mid as i32;
         }
-        return mid as i32;
     }
 }
 
@@ -43,5 +45,17 @@ mod tests {
         let nums = vec![1, 3, 5, 6];
         let target = 7;
         assert_eq!(search_insert_position(nums, target), 4);
+    }
+    #[test]
+    fn test_search_insert_position4() {
+        let nums = vec![1, 3, 5, 6];
+        let target = 0;
+        assert_eq!(search_insert_position(nums, target), 0);
+    }
+    #[test]
+    fn test_search_insert_position5() {
+        let nums = vec![1];
+        let target = 1;
+        assert_eq!(search_insert_position(nums, target), 0);
     }
 }

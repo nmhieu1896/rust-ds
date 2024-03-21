223fc3605cc41906e934010391c0ab1bb6201d63
Hieu Nguyen <nmhieu1896@gmail.com>
2024-03-21 21:09:53 +0700

    setup testg

---
+++ src/algo/binary_search/find_min_in_rotated_sort.rs
@@ -0,0 +1,31 @@
+#[allow(dead_code)]
+pub fn find_min(nums: Vec<i32>) -> i32 {}
+
+#[cfg(test)]
+mod test {
+    use super::find_min;
+
+    #[test]
+    fn test_find_min1() {
+        let nums = vec![3, 4, 5, 1, 2];
+        assert_eq!(find_min(nums), 1);
+    }
+
+    #[test]
+    fn test_find_min2() {
+        let nums = vec![4, 5, 6, 7, 0, 1, 2];
+        assert_eq!(find_min(nums), 0);
+    }
+
+    #[test]
+    fn test_find_min3() {
+        let nums = vec![11, 13, 15, 17];
+        assert_eq!(find_min(nums), 11);
+    }
+
+    #[test]
+    fn test_find_min4() {
+        let nums = vec![1, 2, 3, 4, 5];
+        assert_eq!(find_min(nums), 1);
+    }
+}
--- src/algo/binary_search/mod.rs
+++ src/algo/binary_search/mod.rs
@@ -1,4 +1,5 @@
 mod find_first_and_last;
+mod find_min_in_rotated_sort;
 mod search_in_matrix;
 mod search_in_rotated_sort;
 mod search_insert_position;

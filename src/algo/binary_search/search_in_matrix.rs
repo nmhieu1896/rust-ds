236e1d7a4d66166df48de6b139ac9d6abb80f155
Hieu Nguyen <nmhieu1896@gmail.com>
2024-03-21 20:51:26 +0700

    solution for search_in_matrix

--- src/algo/binary_search/search_in_matrix.rs
+++ src/algo/binary_search/search_in_matrix.rs
@@ -1,6 +1,29 @@
 #[allow(dead_code)]
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    return false;
+    if matrix.len() == 0 {
+        return false;
+    }
+    let rows = matrix.len();
+    let cols = matrix[0].len();
+    let len = rows * cols;
+    let mut left = 0;
+    let mut right = len - 1;
+
+    while left < right {
+        let mid = (left + right) / 2;
+
+        if matrix[mid / cols][mid % cols] < target {
+            left = mid + 1
+        } else {
+            right = mid;
+        }
+    }
+
+    return if matrix[left / cols][left % cols] == target {
+        true
+    } else {
+        false
+    };
 }
 
 #[cfg(test)]
@@ -47,5 +70,12 @@ mod test {
         let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
         let target = 2;
         assert_eq!(search_matrix(matrix, target), false);
+    }
+
+    #[test]
+    fn test_search_matrix7() {
+        let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
+        let target = 20;
+        assert_eq!(search_matrix(matrix, target), true);
     }
 }

8fbd58a5c9c52d65d55f343f3be017e469ba2b22
Hieu Nguyen <nmhieu1896@gmail.com>
2024-03-30 13:25:48 +0700

    better solution for calculator with stack of enum

--- src/algo/stack/calculator_2.rs
+++ src/algo/stack/calculator_2.rs
@@ -3,22 +3,30 @@ enum Mod {
     Add,
     Neg,
 }
+#[derive(Debug)]
+enum StackItem {
+    Value(i32),
+    Operand((), Mod),
+}
+
 #[allow(dead_code)]
 pub fn calculate(s: String) -> i32 {
-    let mut stack: Vec<(Vec<i32>, Mod)> = vec![(vec![], Mod::Add)];
-    let mut num: i32 = 0;
+    let mut stack: Vec<StackItem> = vec![];
+    let mut num: String = String::new();
     let mut curr_mod = Mod::Add;
-    let mut nested_stack = vec![];
 
     s.chars().into_iter().for_each(|char| {
         if char.is_digit(10) {
-            num = num * 10 + char.to_digit(10).unwrap() as i32;
+            if curr_mod == Mod::Neg {
+                num.push('-');
+                curr_mod = Mod::Add;
+            }
+            num.push(char);
             return;
         }
-        if num != 0 {
-            let mul_val = if curr_mod == Mod::Add { 1 } else { -1 };
-            stack.last_mut().unwrap().0.push(num * mul_val);
-            num = 0;
+        if !num.is_empty() {
+            stack.push(StackItem::Value(num.parse::<i32>().unwrap()));
+            num.clear();
         }
 
         match char {
@@ -29,52 +37,46 @@ pub fn calculate(s: String) -> i32 {
                 curr_mod = Mod::Neg;
             }
             c if c == '(' => {
-                // Handle nested mode,
-                // if outer () is Neg, reverse mod for nested ()
-                let nested_mod = match nested_stack.last() {
-                    Some(Mod::Add) => curr_mod.clone(),
-                    Some(Mod::Neg) => match curr_mod {
-                        Mod::Add => Mod::Neg,
-                        Mod::Neg => Mod::Add,
-                    },
-                    None => curr_mod.clone(),
-                };
-                stack.push((vec![], nested_mod.clone()));
-                nested_stack.push(nested_mod);
+                stack.push(StackItem::Operand((), curr_mod.clone()));
                 curr_mod = Mod::Add;
             }
-            c if c == ')' => {
-                nested_stack.pop();
-                // If nested_stack still have item => still in nested ()
-                // Mod must be the mod of outer ().
-                curr_mod = if nested_stack.last().is_some() {
-                    nested_stack.last().unwrap().clone()
-                } else {
-                    Mod::Add
+            c if c == ')' => loop {
+                curr_mod = Mod::Add;
+                let item = stack.pop().unwrap();
+                match item {
+                    StackItem::Value(val) => match stack.last().unwrap() {
+                        StackItem::Operand(_, outer_mod) => {
+                            if *outer_mod == Mod::Neg {
+                                stack.pop().unwrap();
+                                stack.push(StackItem::Value(val * (-1)));
+                            } else {
+                                stack.pop().unwrap();
+                                stack.push(StackItem::Value(val));
+                            }
+                            break;
+                        }
+                        StackItem::Value(pre_val) => {
+                            *stack.last_mut().unwrap() = StackItem::Value(val + pre_val)
+                        }
+                    },
+                    _ => {}
                 };
-                stack.push((vec![], curr_mod.clone()));
-            }
+            },
             _ => {}
         }
     });
-    if num != 0 {
+    if !num.is_empty() {
         let mul_val = if curr_mod == Mod::Add { 1 } else { -1 };
-        stack.last_mut().unwrap().0.push(num * mul_val);
+        stack.push(StackItem::Value(num.parse::<i32>().unwrap() * mul_val));
     }
 
-    let mut sum: i32 = 0;
     stack
         .iter()
-        .map(|item| (item.0.iter().sum::<i32>(), item.1.clone()))
-        .for_each(|(value, curr_mod)| match curr_mod {
-            Mod::Add => {
-                sum += value;
-            }
-            Mod::Neg => {
-                sum -= value;
-            }
-        });
-    return sum;
+        .map(|item| match item {
+            StackItem::Value(val) => *val,
+            _ => 0,
+        })
+        .sum()
 }
 
 #[cfg(test)]

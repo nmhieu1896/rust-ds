c498853a6827c3969328108ad6ec1025779069dd
Hieu Nguyen <nmhieu1896@gmail.com>
2024-04-20 22:18:07 +0700

    faster close string

--- src/algo/hash_map/close_strings.rs
+++ src/algo/hash_map/close_strings.rs
@@ -1,34 +1,26 @@
-use std::collections::{HashMap, HashSet};
 #[allow(dead_code)]
 pub fn close_strings(word1: String, word2: String) -> bool {
     if word1.len() != word2.len() {
         return false;
     }
 
-    let mut map1 = HashMap::new();
-    let mut map2 = HashMap::new();
-    // let mut set1 = HashSet::new();
-    // let mut set2 = HashSet::new();
-    let mut vec1 = vec![];
-    let mut vec2 = vec![];
+    let mut map1 = [0; 26];
+    let mut map2 = [0; 26];
     for c in word1.bytes() {
-        *map1.entry(c).or_insert(0) += 1;
-        // set1.insert(c);
+        map1[(c - b'a') as usize] += 1;
     }
     for c in word2.bytes() {
-        *map2.entry(c).or_insert(0) += 1;
-        // set1.insert(c);
+        map2[(c - b'a') as usize] += 1;
     }
-    for (k, v1) in map1 {
-        vec1.push(v1);
-        if !map2.contains_key(&k) {
+    for i in 0..26 {
+        if (map1[i] == 0 && map2[i] != 0) || (map1[i] != 0 && map2[i] == 0) {
             return false;
         }
-        vec2.push(map2[&k]);
     }
-    vec1.sort();
-    vec2.sort();
-    return vec1 == vec2;
+
+    map1.sort();
+    map2.sort();
+    return map1 == map2;
 }
 
 #[cfg(test)]

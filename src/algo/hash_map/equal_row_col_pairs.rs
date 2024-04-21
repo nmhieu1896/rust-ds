563bd7b2d7c3cc10c24a58d6028c33d0fb6c1992
Hieu Nguyen <nmhieu1896@gmail.com>
2024-04-21 20:50:26 +0700

    equal row and col pairs

---
+++ src/algo/hash_map/equal_row_col_pairs.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+#[allow(dead_code)]
+pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
+    let mut map = HashMap::new();
+    let mut count = 0;
+    for v in grid.iter() {
+        let mut k = String::new();
+        v.iter().for_each(|x| {
+            k.push_str(&x.to_string());
+            k.push('-');
+        });
+        *map.entry(k).or_insert(0) += 1;
+    }
+
+    for i in 0..grid.len() {
+        let mut k = String::new();
+        for j in 0..grid.len() {
+            k.push_str(&grid[j][i].to_string());
+            k.push('-');
+        }
+        if let Some(inc) = map.get(&k) {
+            count += inc;
+        }
+    }
+
+    return count;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_equal_pairs1() {
+        let grid = vec![vec![3, 2, 1], vec![1, 7, 6], vec![2, 7, 7]];
+        assert_eq!(equal_pairs(grid), 1);
+    }
+
+    #[test]
+    fn test_equal_pairs2() {
+        let grid = vec![
+            vec![3, 1, 2, 2],
+            vec![1, 4, 4, 5],
+            vec![2, 4, 2, 2],
+            vec![2, 4, 2, 2],
+        ];
+        assert_eq!(equal_pairs(grid), 3);
+    }
+    #[test]
+    fn test_equal_pairs3() {
+        let grid = vec![vec![1, 1], vec![1, 1]];
+        assert_eq!(equal_pairs(grid), 4);
+    }
+    #[test]
+    fn test_equal_pairs4() {
+        let grid = vec![vec![11, 1], vec![1, 11]];
+        assert_eq!(equal_pairs(grid), 2);
+    }
+}
--- src/algo/hash_map/mod.rs
+++ src/algo/hash_map/mod.rs
@@ -1,2 +1,3 @@
 mod close_strings;
+mod equal_row_col_pairs;
 mod find_diff_of_2_arrays;

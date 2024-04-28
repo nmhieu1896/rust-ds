d4c24696c001385af7a27e17853aa5f4d59f2f4c
Hieu Nguyen <nmhieu1896@gmail.com>
2024-04-28 09:37:13 +0700

    shortest way to form a string

--- src/algo/hash_map/shortest_way_to_form_string.rs
+++ src/algo/hash_map/shortest_way_to_form_string.rs
@@ -1,6 +1,31 @@
 #[allow(dead_code)]
 pub fn shortest_way(source: String, target: String) -> i32 {
-    return 0;
+    let mut map = std::collections::HashMap::new();
+    let mut result = 0;
+    for (i, c) in source.bytes().enumerate() {
+        map.entry(c).or_insert(vec![]).push(i);
+    }
+    let mut idx = -1;
+    for (i, c) in target.bytes().enumerate() {
+        if !map.contains_key(&c) {
+            return -1;
+        }
+        let val = map.get(&c).unwrap();
+        let next_idx = val.iter().find(|&&x| (x as i32) > idx);
+
+        if next_idx.is_none() {
+            result += 1;
+            idx = *val.get(0).unwrap() as i32;
+        } else {
+            idx = *next_idx.unwrap() as i32;
+        }
+
+        if i == target.len() - 1 {
+            result += 1;
+        }
+    }
+
+    return result;
 }
 
 #[cfg(test)]
@@ -26,5 +51,13 @@ mod test {
         let source = "xyz";
         let target = "xzyxz";
         assert_eq!(shortest_way(source.to_string(), target.to_string()), 3);
+    }
+
+    #[test]
+    fn test_shortest_way4() {
+        let source = "bxdisnclwdrpcqamhqqvudgtdbsdikhzzbnlgzlspozvhdkunxkpevnqvyrfowanagolpwvezuvnhgxvjopcvrkdaippmwgkofbo";
+        let target =
+            "ntzebqmlrzxissncdlvcxbojgbnnphtfdunjpzroegfdvieaajafjkidpxbrgsjpgmalekhjckwgygfz";
+        assert_eq!(shortest_way(source.to_string(), target.to_string()), 17);
     }
 }

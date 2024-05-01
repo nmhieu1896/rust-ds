435677b75c989af2d415fda7f6092e08347d5dfa
Hieu Nguyen <nmhieu1896@gmail.com>
2024-05-01 16:09:14 +0700

    parse json primitive

--- src/parser/json.rs
+++ src/parser/json.rs
@@ -2,22 +2,59 @@ use std::collections::HashMap;
 
 #[allow(dead_code)]
 fn parser(str: String) -> Json {
-    // str.chars().for_each(|c| {
-    //     if c.is_digit(10) {
-    //         return Json::Number(c.to_digit(10).unwrap() as f64);
-    //     }
-    //     if c.is_alphabetic() {
-    //         return Json::String(c.to_string());
-    //     }
-    // });
+    let mut iter_str = str.chars();
+    loop {
+        let c = iter_str.next().unwrap();
+        let mut new_str = String::new();
 
-    return Json::Boolean(false);
+        if c.is_digit(10) {
+            new_str.push(c);
+            loop {
+                let next_c = iter_str.next();
+                if (next_c.is_some() && next_c.unwrap().is_digit(10)) || next_c == Some('.') {
+                    new_str.push(next_c.unwrap());
+                } else {
+                    break;
+                }
+            }
+            return Json::Number(new_str.parse::<f64>().unwrap());
+        }
+        if c == '"' {
+            loop {
+                let next_c = iter_str.next();
+                if next_c.is_none() || next_c == Some('"') {
+                    break;
+                } else {
+                    new_str.push(next_c.unwrap());
+                }
+            }
+            return Json::String(new_str.to_string());
+        }
+        if c == 't' {
+            new_str.push(c);
+            for _ in 0..3 {
+                new_str.push(iter_str.next().unwrap());
+            }
+            if new_str == "true" {
+                return Json::Boolean(true);
+            }
+        }
+        if c == 'f' {
+            new_str.push(c);
+            for _ in 0..4 {
+                new_str.push(iter_str.next().unwrap());
+            }
+            if new_str == "false" {
+                return Json::Boolean(false);
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 enum Json {
-    Object(HashMap<String, Json>),
-    Array(Vec<Json>),
+    Object(HashMap<String, Box<Json>>),
+    Array(Vec<Box<Json>>),
     Number(f64),
     String(String),
     Boolean(bool),
@@ -29,7 +66,7 @@ mod test {
 
     #[test]
     fn test_parser1() {
-        let str = r#"Hello world"#;
+        let str = r#""Hello world""#;
         assert_eq!(
             parser(str.to_string()),
             Json::String("Hello world".to_string())
@@ -37,12 +74,15 @@ mod test {
     }
     #[test]
     fn test_parser2() {
-        let str = r#"1024"#;
+        let str = "1024";
         assert_eq!(parser(str.to_string()), Json::Number(1024 as f64));
     }
-    // #[test]
-    // fn test_parser1() {
-    //   let str = r#"{ "name": "John", "age": 30, "car": null }"#;
-    //   assert_eq!(parser(str.to_string()), Json::Object(HashMap::new()));
-    // }
+
+    #[test]
+    fn test_parser3() {
+        let str1 = "true";
+        let str2 = "false";
+        assert_eq!(parser(str1.to_string()), Json::Boolean(true));
+        assert_eq!(parser(str2.to_string()), Json::Boolean(false));
+    }
 }

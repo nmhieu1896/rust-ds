73e63710246941815aba0adac2d31bc2235931f0
Hieu Nguyen <nmhieu1896@gmail.com>
2024-05-06 23:10:22 +0700

    why i'm so stupid

---
+++ src/algo/others/missing_numbers.rs
@@ -0,0 +1,40 @@
+pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
+    let mut res = vec![];
+    let mut nums = nums;
+    nums.sort();
+    let mut prev = 0;
+    for &v in nums.iter() {
+        if v - 1 > prev {
+            for i in prev + 1..v {
+                res.push(i);
+            }
+        }
+        prev = v;
+    }
+    if nums[nums.len() - 1] < nums.len() as i32 {
+        for i in nums[nums.len() - 1] + 1..=nums.len() as i32 {
+            res.push(i);
+        }
+    }
+
+    return res;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_find_disappeared_numbers1() {
+        let nums = vec![4, 3, 2, 7, 8, 2, 3, 1];
+        let res = vec![5, 6];
+        assert_eq!(find_disappeared_numbers(nums), res);
+    }
+
+    #[test]
+    fn test_find_disappeared_numbers2() {
+        let nums = vec![1, 1];
+        let res = vec![2];
+        assert_eq!(find_disappeared_numbers(nums), res);
+    }
+}
--- src/algo/others/mod.rs
+++ src/algo/others/mod.rs
@@ -1,1 +1,2 @@
+mod missing_numbers;
 mod place_flower;

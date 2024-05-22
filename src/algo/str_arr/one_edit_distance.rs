85c72425d1c11472f76734ae637ca9620aef5485
Hieu Nguyen <nmhieu1896@gmail.com>
2024-05-22 22:13:51 +0700

    one_edit_distance: remove delete case by recursion

--- src/algo/str_arr/one_edit_distance.rs
+++ src/algo/str_arr/one_edit_distance.rs
@@ -1,43 +1,39 @@
 #[allow(dead_code)]
 pub fn is_one_edit_distance(s: String, t: String) -> bool {
+    if s.len() > t.len() {
+        return is_one_edit_distance(t, s);
+    }
+    if t.len() - s.len() > 1 {
+        return false;
+    }
+
     let mut change = 0;
-    let mut s1 = s.chars();
-    let mut t1 = t.chars();
-    let mut c1 = s1.next();
-    let mut c2 = t1.next();
+    let mut s_iter = s.chars();
+    let mut t_iter = t.chars();
+    let mut c_s = s_iter.next();
+    let mut c_t = t_iter.next();
 
     loop {
-        if c1.is_none() && c2.is_none() {
+        if c_s.is_none() && c_t.is_none() {
             break;
         }
-        // one of the iterators is exhausted
-        if (c1.is_none() && c2.is_some()) || (c1.is_some() && c2.is_none()) {
-            change += 1;
-            c2 = t1.next();
-            c1 = s1.next();
-            continue;
-        }
-        // equal
-        if c1 == c2 {
-            c2 = t1.next();
-            c1 = s1.next();
+        // equal, both are Some
+        if c_s == c_t {
+            c_t = t_iter.next();
+            c_s = s_iter.next();
             continue;
         }
         // not equal
         change += 1;
-        // replace
-        if s.len() == t.len() {
-            c2 = t1.next();
-            c1 = s1.next();
+
+        // one is none (insert) or same length
+        if c_t.is_none() || (s.len() == t.len()) {
+            c_s = s_iter.next();
+            c_t = t_iter.next();
+            continue;
         }
-        // delete
-        if s.len() > t.len() {
-            c1 = s1.next()
-        }
-        // insert
-        if s.len() < t.len() {
-            c2 = t1.next();
-        }
+
+        c_t = t_iter.next();
     }
 
     return change == 1;
